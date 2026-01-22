117c8caa06b95036316cb187140732f747786c20
Mohana-priya-7 <s.mohanapriya2174@gmail.com>
2026-01-22 11:05:51 +0530

    This is project

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
-    username=models.CharField(max_length=150, unique=True)
+    username=models.CharField(max_length=50)
     email=models.EmailField(unique=True)
     USERNAME_FIELD='email'
     REQUIRED_FIELDS=['username']
